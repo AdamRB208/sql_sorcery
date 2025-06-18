46c21384d5301778664805f22de4e99912f40f17
AdamRB208 <adamrborup@gmail.com>
2025-06-18 15:32:49 -0600

    init

---
+++ server/Globals.cs
@@ -0,0 +1,16 @@
+global using System;
+global using System.Data;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using Dapper;
+global using CodeWorks.Utils;
+
+// APPLICATION SPECIFIC
+global using sql_sorcery.Repositories;
+global using sql_sorcery.Services;
+global using sql_sorcery.Models;
---
+++ server/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace sql_sorcery.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
